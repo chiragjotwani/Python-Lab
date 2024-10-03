53519b3885e49d096eb94b99f66b864b41ebb9f8
Chirag Jotwani <chirag.jotwaniofficial@gmail.com>
2024-10-03 09:06:55 +0530

    Update Convert_to_stirng.py

--- OOPS lab-5/Convert_to_stirng.py
+++ OOPS lab-5/Convert_to_stirng.py
@@ -1,8 +1,8 @@
-list = [1,2,3,4,5]
-tuple = (6,7,8,9,10)
+list1 = [1,2,3,4,5]
+tuple1 = (6,7,8,9,10)
 
-strings_from_list = list(map(str, list))
-strings_from_tuple = list(map(str, tuple))
+strings_from_list = list(map(str, list1))
+strings_from_tuple = list(map(str, tuple1))
 
 combined_strings= strings_from_list + strings_from_tuple
 
